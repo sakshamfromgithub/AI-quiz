bfb5d8db55d0656e1c72390f86bbcaa6d421f68a
SAKSHAM SHRIVASTAV <167529365+sakshamfromgithub@users.noreply.github.com>
2026-03-31 13:40:25 +0530

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -21,8 +21,8 @@ Difficulty: ${difficulty}
 `;
 
   try {
-    const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" + process.env.GEMINI_API_KEY,
+  const response = await fetch(
+  "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
